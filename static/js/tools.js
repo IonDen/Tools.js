fe4218e2eb60de3a4eef8e060372992c6dc50781
Denis <denis.ineshin@gmail.com>
2012-08-26 17:49:11 +0400

    Added index file and css

---
+++ static/js/tools.js
@@ -0,0 +1,45 @@
+﻿// Tools.js
+// version 1.0.55
+// Copyright 2012, Denis Ineshin
+// http://ionden.com/tools/
+// Released under the MIT license.
+// http://ionden.com/tools/license.htm
+// =====================================================================================================================
+
+var tools = tools || {};
+
+// =====================================================================================================================
+// tools.mobile, rev: 5
+
+tools.mobile = function(){
+    var n = navigator.userAgent;
+    var devices = [
+        /Android/i,
+        /iPhone/i,
+        /iPod/i,
+        /iPad/i,
+        /BlackBerry/i,
+        /PlayBook/i,
+        /Windows Phone/i
+    ];
+
+    var result = false;
+    for(var i = 0; i < devices.length; i++){
+        result = devices[i].test(n);
+        if(result) break;
+    }
+    tools.mobile = result;
+    return result;
+};
+
+
+
+
+// =====================================================================================================================
+// Launch code
+
+$(document).ready(function(){
+
+    tools.mobile(); // it is now boolean
+
+});
