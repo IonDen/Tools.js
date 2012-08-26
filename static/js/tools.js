eae1bcdbe76647c32fa22c0922aa3facd97d5812
Denis <denis.ineshin@gmail.com>
2012-08-26 20:00:26 +0400

    Tools and readme update

--- static/js/tools.js
+++ static/js/tools.js
@@ -1,5 +1,5 @@
 ﻿// Tools.js
-// version 1.0.56
+// version 1.0.57
 // Copyright 2012, Denis Ineshin
 // http://ionden.com/tools/
 // Released under the MIT license.
@@ -33,6 +33,42 @@ tools.mobile = function(){
 };
 
 
+// =====================================================================================================================
+// tools.fuck, rev: 7
+
+tools.fuck = {
+    init: function(){
+        this.patterns = [
+            /х(уй|уи|уе|уё|ую)/gi,
+            /x(uy|ui|yi|yu|ye|ue)/gi,
+            /бля(ть|дь)|блеа(ть|дь)|блиа(ть|дь)|блия(ть|дь)|бл[яе](ди|до)|бля(?=(\s\S|$|\d))|\Sбля|бля\S/gi,
+            /bl(i|e|y|u)(a|at|yt)/gi,
+            /п(и|е)(зд|сд|ст|зт)(о|ю|а|ец|у|и|я)/gi,
+            /p[ie][zs]d(ec|a|u|y)/gi,
+            /су(чк|ка|ку|чар|чеч|чёч|чоч)/gi,
+            /s[uy](ka|ch|4)/gi,
+            /(еб|ёб)(ал|ат|ис|ан|ла|уч|ок|ки|у(?=(\s|$|\d))|и(?=(\s|$|\d)))/gi,
+            /eb\b|eb(al|at|is|it|la)/gi,
+            /(еб(?=(\s|$|\d))|ёб(?=(\s|$|\d)))/gi,
+            /г[ао]ндон|муда[кч]|чмо|4мо/gi,
+            /g[ao]ndon|muda[kc]|сhmo|4mo/gi,
+            /пр[ао]шм[ао]ндовк(у|а|и)/gi,
+            /pr[ao]s[h]?mandovk[aiu]?/gi,
+            /пид[ао]?р/gi,
+            /p[ie]d[ao]?r/gi,
+            /fuck|bitch|asshole|dick/gi
+        ];
+    },
+    check: function(text){
+        this.text = text;
+        for(var i = 0; i < this.patterns.length; i++){
+            this.text = this.text.replace(this.patterns[i],"*");
+        }
+        return this.text;
+    }
+};
+
+
 
 
 // =====================================================================================================================
@@ -41,5 +77,6 @@ tools.mobile = function(){
 tools.loadComponents = function(target, isContext){
 
     tools.mobile(); // it is now boolean
+    tools.fuck.init();
 
 };
